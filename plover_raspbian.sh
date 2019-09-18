cd21d45ff3badcc93aa1ecc47a4ba14a5647ecaf
Stanley Sakai <stanley@stanographer.com>
2019-09-18 16:59:09 -0700

    remove dev tools

--- plover_raspbian.sh
+++ plover_raspbian.sh
@@ -5,13 +5,13 @@ sudo apt update
 sudo apt install 2to3
 
 # Install PyQt5
-sudo apt install -y python3-pyqt5 pyqt5-devtools python-sip python-sip-dev python3-babel
+sudo apt install -y python3-babel
 
 # Install pip.
 sudo apt install pipenv
 
 # Make PyQt5 and SIP.
-cd /usr/src
+cd /tmp
 sudo wget https://www.riverbankcomputing.com/static/Downloads/sip/4.19.13/sip-4.19.13.tar.gz
 sudo wget https://www.riverbankcomputing.com/static/Downloads/PyQt5/5.13.1/PyQt5_gpl-5.13.1.tar.gz
 
@@ -21,15 +21,12 @@ sudo python3 configure.py --sip-module PyQt5.sip
 sudo make
 sudo make install
 
-cd ..
-
+cd /tmp
 sudo tar xzf PyQt5_gpl-5.13.1.tar.gz
 cd PyQt5_gpl-5.13.1
 sudo python3 configure.py
 sudo make
 sudo make install
-
-
 
 cd ~/plover-pi
 
