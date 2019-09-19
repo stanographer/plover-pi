af3e2acf8539032311743d904348fdc520ad23f7
Stanley Sakai <stanley@stanographer.com>
2019-09-19 11:13:01 -0700

    downgraded sip and pyqt

--- plover_raspbian.sh
+++ plover_raspbian.sh
@@ -12,22 +12,23 @@ sudo apt install pipenv
 
 # Make PyQt5 and SIP.
 cd /tmp
-sudo wget https://www.riverbankcomputing.com/static/Downloads/sip/4.19.19/sip-4.19.19.tar.gz
-sudo wget https://www.riverbankcomputing.com/static/Downloads/PyQt5/5.13.1/PyQt5_gpl-5.13.1.tar.gz
+sudo wget https://www.riverbankcomputing.com/static/Downloads/sip/4.19.13/sip-4.19.13.tar.gz
+sudo wget https://www.riverbankcomputing.com/static/Downloads/PyQt5/5.11.3/PyQt5_gpl-5.11.3.tar.gz
 
-sudo tar xzf sip-4.19.19.tar.gz
-cd sip-4.19.19
-sudo python3 configure.py --sip-module PyQt5.sip
+sudo tar xzf sip-4.19.13.tar.gz
+cd sip-4.19.13
+sudo python3 configure.py --sip-module=PyQt5.sip
 sudo make
 sudo make install
 
 cd /tmp
-sudo tar xzf PyQt5_gpl-5.13.1.tar.gz
-cd PyQt5_gpl-5.13.1
+sudo tar xzf PyQt5_gpl-5.11.3.tar.gz
+cd PyQt5_gpl-5.11.3
 sudo python3 configure.py
 sudo make
 sudo make install
 
+# Return to repo dir.
 cd ~/plover-pi
 
 # Clone latest Plover repo.
@@ -35,6 +36,10 @@ git clone git@github.com:openstenoproject/plover.git plover/
 
 # Get into plover.
 cd plover
+
+sudo pip3 install pydbus
+sudo usermod -a -G uucp pi
+sudo usermod -a -G dialout pi
 
 # Install Plover.
 pip3 install -r requirements.txt
